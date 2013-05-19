6369e6d067595e96df38de0cbfad020dd4e20357
millecker <martin.illecker@gmaill.com>
2013-05-19 21:17:03 +0200

    Modified TestGpuBSP (NullWritable)

--- src/edu/syr/pcpratts/rootbeer/testcases/rootbeertest/serialization/hama/TestGpuBSP.java
+++ src/edu/syr/pcpratts/rootbeer/testcases/rootbeertest/serialization/hama/TestGpuBSP.java
@@ -7,29 +7,28 @@
 
 package edu.syr.pcpratts.rootbeer.testcases.rootbeertest.serialization.hama;
 
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hama.bsp.BSPPeer;
 import org.apache.hama.bsp.gpu.GpuBSP;
 
-public class TestGpuBSP extends GpuBSP<Text, Text, Text, Integer, IntWritable> {
+public class TestGpuBSP extends GpuBSP<NullWritable, NullWritable, NullWritable, NullWritable, NullWritable> {
 
   public TestGpuBSP() {
     super();
   }
 
   @Override
-  public void setupGPU(BSPPeer<Text, Text, Text, Integer, IntWritable> peer){
+  public void setupGPU(BSPPeer<NullWritable, NullWritable, NullWritable, NullWritable, NullWritable> peer){
     System.out.println("Rootbeer TestGpuBSP setupGPU!");
   }
 
   @Override
-  public void bspGPU(BSPPeer<Text, Text, Text, Integer, IntWritable> peer){
+  public void bspGPU(BSPPeer<NullWritable, NullWritable, NullWritable, NullWritable, NullWritable> peer){
     System.out.println("Rootbeer TestGpuBSP bspGPU!");
   }
 
   @Override
-  public void cleanupGPU(BSPPeer<Text, Text, Text, Integer, IntWritable> peer){
+  public void cleanupGPU(BSPPeer<NullWritable, NullWritable, NullWritable, NullWritable, NullWritable> peer){
     System.out.println("Rootbeer TestGpuBSP cleanupGPU!");
   }
 }
