89d790f0fa048b8e55d33cf292de385d0ceeb7ee
millecker <martin.illecker@gmaill.com>
2013-05-22 12:39:05 +0200

    Fixed calling a host function("printf") from a device function issue
    Calling a __host__ function("printf") from a __device__
    function("java_lang_String__array_get") is not allowed

--- src/edu/syr/pcpratts/rootbeer/entry/DontDfsMethods.java
+++ src/edu/syr/pcpratts/rootbeer/entry/DontDfsMethods.java
@@ -78,15 +78,8 @@ public class DontDfsMethods {
     m_methods.add("<java.lang.StringValue: char[] 'from'(char[])>");
     m_methods.add("<java.util.Arrays: java.lang.Object[] copyOf(java.lang.Object[],int)>");
     m_methods.add("<java.io.PrintStream: void println(java.lang.String)>");
-    //Apache Hama Native Implementation
+    // Apache Hama Native Implementation
     m_methods.add("<org.apache.hama.bsp.BSPPeer: java.lang.String[] getAllPeerNames()>");
-    /* invoke_org_apache_hama_bsp_BSPPeer_getAllPeerNames */
-    /*
-    __device__ int org_apache_hama_bsp_BSPPeer_getAllPeerNames( char * gc_info, int thisref, int * exception){
-      return 0;
-    }
-    */
-
     
   }
   
