f300217b8c4af30a8015a79410cc76ef579665ee
millecker <martin.illecker@gmail.com>
2013-12-13 10:49:01 +0100

    Modified AutoboxingTest

--- src/edu/syr/pcpratts/rootbeer/testcases/rootbeertest/serialization/AutoboxingRunOnGpu.java
+++ src/edu/syr/pcpratts/rootbeer/testcases/rootbeertest/serialization/AutoboxingRunOnGpu.java
@@ -4,25 +4,42 @@ import edu.syr.pcpratts.rootbeer.runtime.Kernel;
 
 public class AutoboxingRunOnGpu implements Kernel {
 
-  private Double m_result;
+  private Double m_double_result;
+  private Integer m_interger_result;
+  
   public void gpuMethod() {
-    m_result = returnDouble();
+    m_double_result = returnDouble();
+    m_interger_result = returnInteger();
   }
 
   private double returnDouble() {
     return 10;
   }
   
-  public double getResult(){
-    return m_result;
+  private int returnInteger() {
+    return 0; // values between -128 and 0 will fail
+  }
+  
+  public double getDoubleResult(){
+    return m_double_result;
+  }
+  
+  public double getIntegerResult(){
+    return m_interger_result;
   }
 
   public boolean compare(AutoboxingRunOnGpu rhs) {
     try {
-      if(getResult() != rhs.getResult()){
-        System.out.println("m_result");
-        System.out.println("lhs: "+getResult());
-        System.out.println("rhs: "+rhs.getResult());
+      if(getDoubleResult() != rhs.getDoubleResult()){
+        System.out.println("m_double_result");
+        System.out.println("lhs: "+getDoubleResult());
+        System.out.println("rhs: "+rhs.getDoubleResult());
+        return false;
+      }
+      if(getIntegerResult() != rhs.getIntegerResult()){
+        System.out.println("m_interger_result");
+        System.out.println("lhs: "+getIntegerResult());
+        System.out.println("rhs: "+rhs.getIntegerResult());
         return false;
       }
       return true;
