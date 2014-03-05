b2c38cd42447d0f7967f7a2c2b4ee751b1ed5f48
millecker <martin.illecker@gmail.com>
2014-03-05 15:21:49 +0100

    Add next point in KeyValuePair for linked list

--- src/org/trifort/rootbeer/runtime/KeyValuePair.java
+++ src/org/trifort/rootbeer/runtime/KeyValuePair.java
@@ -24,26 +24,35 @@ public class KeyValuePair {
 
   private Object m_key;
   private Object m_value;
+  private KeyValuePair m_next;
 
   public KeyValuePair(Object key, Object value) {
     this.m_key = key;
     this.m_value = value;
-  }
-
-  public Object getKey() {
-    return m_key;
-  }
-
-  public Object getValue() {
-    return m_value;
+    this.m_next = null;
   }
 
   public void setKey(Object key) {
     this.m_key = key;
   }
 
+  public Object getKey() {
+    return m_key;
+  }
+
   public void setValue(Object value) {
     this.m_value = value;
   }
 
+  public Object getValue() {
+    return m_value;
+  }
+
+  public void setNext(KeyValuePair next) {
+    this.m_next = next;
+  }
+
+  public KeyValuePair getNext() {
+    return m_next;
+  }
 }
